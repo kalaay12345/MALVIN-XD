9606a86791415fb3d7df77843c0999652e13a75c
K H 4 N <khanmaxamed22@gmail.com>
2025-08-04 07:51:44 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~sjVkBAhD#6P954v06uF5QTp9tz0VyUZk6ZhFkvOwoIQjx6GU8pRk ",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
