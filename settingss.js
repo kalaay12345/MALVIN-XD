26e3997a3237e6995b110639f175048e04826385
K H 4 N <khanmaxamed22@gmail.com>
2025-08-09 15:00:09 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "malvin~UuEDUQ7S#Fg8Dop7jEUXvh__rdClL3Gen9XGR9BL7dwBv7W125t4",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~dysTlaTZ#NOp8qVcCkeC-PU4Qz3aUZHce4BWIanTfp1VxfdG7imI",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
