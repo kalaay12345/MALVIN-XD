22464d2f49f2d190134361e63fde8386af7c0bc3
K H 4 N <khanmaxamed22@gmail.com>
2025-08-28 18:52:09 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~ZytBRaCJ#4p4iD1p6N0Kf3kDrJt8KSDBTwMStSL4F57QIJzNxwVk",  // add your Session Id here
+  SESSION_ID: "malvin~AqtzhJhC#Q8v9nJdBNIzNMyib6MspU-srOwIgJl_JfUw94Zt5OAU",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
